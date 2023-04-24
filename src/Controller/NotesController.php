12e3bcf5c85d5be905adcfd67b6e7287ecb1bfdc
Tommy Dumora <tommydumora@gmail.com>
2023-04-24 12:01:13 +0200

    modif Note.php

---
+++ src/Controller/NotesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\NoteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NotesController extends AbstractController
+{
+    #[Route('/notes', name: 'app_notes')]
+    public function index(NoteRepository $noteRepository): Response
+    {
+        $notes = $noteRepository->findAll();
+        return $this->render('notes/index.html.twig', [
+            'notes' => $notes,
+        ]);
+    }
+}
